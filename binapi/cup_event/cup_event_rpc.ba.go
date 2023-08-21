1dd4567b205c5e8c72d631e18125da8f3ef3aad0
root <root@localhost.localdomain>
2023-08-21 19:33:18 +0800

    fix cup package path

--- binapi/cup_event/cup_event_rpc.ba.go
+++ binapi/cup_event/cup_event_rpc.ba.go
@@ -5,7 +5,7 @@ package cup_event
 import (
 	"context"
 
-	api "git.fd.io/govpp.git/api"
+	api "go.fd.io/govpp/api"
 )
 
 // RPCService defines RPC service cup_event.
--- binapi/cup_ipfilter/cup_ipfilter_rpc.ba.go
+++ binapi/cup_ipfilter/cup_ipfilter_rpc.ba.go
@@ -5,7 +5,7 @@ package cup_ipfilter
 import (
 	"context"
 
-	api "git.fd.io/govpp.git/api"
+	api "go.fd.io/govpp/api"
 )
 
 // RPCService defines RPC service cup_ipfilter.
